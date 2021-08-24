539cbf334f5e22c2a51448f23dfaee6ba66a8f64
Predrag Kisic <77865282+Pkisic@users.noreply.github.com>
2021-08-24 13:16:15 +0200

    FIN-15

---
+++ aplikacija/toDoAPP/Models/Obaveze.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace toDoAPP.Models
+{
+    public partial class Obaveze
+    {
+        public int? ObavezaId { get; }
+        public string Naziv { get; set; }
+        public string Opis { get; set; }
+        public DateTime DatIzvrsenja { get; set; }
+        public string Kategorija { get; set; }
+        
+    }
+}
---
+++ aplikacija/toDoAPP/Service/IObavezeService.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using toDoAPP.Models;
+
+namespace toDoAPP.Service
+{
+    public interface IObavezeService
+    {
+        IEnumerable<Obaveze> GetAll();
+        void napraviObavezu(Obaveze novaObaveza);
+        ContentResult proveriPodatke(Obaveze novaObaveza);
+    }
+}
